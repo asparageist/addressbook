82a029d01e6279a9d8ea2055c9f583ccef17f0a2
Joseph Murray <josephwmurray@gmail.com>
2023-07-02 14:52:03 -0700

    update to end of day's lessons

--- js/scripts.js
+++ js/scripts.js
@@ -43,6 +43,20 @@ Contact.prototype.fullName = function(){
 //UI LOGIC
 let addressBook = new AddressBook();
 
+function listContacts(addressBookToDisplay) {
+  let contactsDiv = document.querySelector("div#contacts");
+  contactsDiv.innerText = null;
+  const ul = document.createElement("ul");
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    const li = document.createElement("li");
+    li.append(contact.fullName());
+    li.setAttribute("id", contact.id);
+    ul.append(li);
+  });
+  contactsDiv.append(ul);
+}
+
 function handleFormSub(event) {
   event.preventDefault();
   const inputtedFirstName = document.querySelector("input#new-first-name").value;
@@ -50,7 +64,7 @@ function handleFormSub(event) {
   const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
   let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
   addressBook.addContact(newContact);
-  console.log(addressBook.contacts);
+  listContacts(addressBook);
 }
 
 window.addEventListener("load", function() {
